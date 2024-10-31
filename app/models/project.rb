b8d2a1dd4aadb587c8bec2201c5598d24496588a
Thiago Assis <thiagosil@gmail.com>
2024-10-31 19:44:03 -0300

    feat: List both created and shared projects on projects index API

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,8 @@ class Project < ApplicationRecord
 
   has_many :documents, dependent: :destroy
   has_many :reports, dependent: :destroy
-  
+  has_many :shared_resources, as: :shareable, dependent: :destroy
+
   validates :name, presence: true
   validates :description, presence: true
 end
